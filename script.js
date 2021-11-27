a55b92103f7fea31a76406d9dca6be493aa8d6c4
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-11-27 19:07:39 -0300

    navigation section css and dark mode button js

---
+++ script.js
@@ -0,0 +1,11 @@
+const theme = document.getElementById('theme'),
+    body = document.body;
+theme.addEventListener('click', () => {
+    if (theme.classList.contains('ativo')) {
+        theme.classList.remove('ativo');
+        document.body.classList.remove('escuro');
+    } else {
+        theme.classList.add('ativo');
+        document.body.classList.add('escuro');
+    }
+})
